e723caa1c4126502048f7f43b80c08c770a07b08
beskorovainyi <beskorovainyi.max@gmail.com>
2021-09-28 23:43:06 +0300

    ADDED:added register form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Auth from "./layouts/Auth";
 import Dashboard from "./layouts/Dashboard";
 
 const App = () => {
-
 
   return (
       <BrowserRouter>
         <Switch>
           <Route path="/auth" component={(props) => <Auth {...props} />}/>
           <Route path="/dashboard" component={(props) => <Dashboard {...props} />}/>
+          <Redirect from="/" to="/auth" />
         </Switch>
       </BrowserRouter>
   );
--- src/layouts/Auth.js
+++ src/layouts/Auth.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {useState} from "react";
 
 // react-bootstrap components
 // import {
@@ -17,9 +18,34 @@ import React from "react";
 
 const Auth = () => {
 
+  const [firstName, setFirstName] = useState("");
+  const [registerFirstName, setRegisterFirstName] = useState(true);
+
   return (
       <div>
         <h1>Auth Page</h1>
+
+        <Form.Group className={
+          "has-label " +
+          (registerFirstNameState ? "has-success" : "has-error")
+        }>
+          <span className="star">*</span>
+          <label>First Name</label>
+          <Form.Control
+              value={firstName}
+              placeholder="Your First Name"
+              type="text"
+              onChange={(event) => {
+                setFirstName(event.target.value);
+                if (minLength(event.target.value, 1)) {
+                  setRegisterFirstNameState(true);
+                } else {
+                  setRegisterFirstNameState(false);
+                }
+              }}
+          ></Form.Control>
+        </Form.Group>
+
       </div>
   )
 }
